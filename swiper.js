7ccfd735586642771fb27962fa4b7201945166f4
yarynkaaaa <usia98200@gmail.com>
2025-03-15 22:59:14 +0200

    add all files and papcki

---
+++ swiper.js
@@ -0,0 +1,14 @@
+const carousel = document.querySelector(".carousel");
+const cards = document.querySelectorAll(".card");
+let cardWidth = cards[0].offsetWidth + 20; // Ширина картки + відступ
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = cards.length - 1;
+    } else if (currentIndex >= cards.length) {
+        currentIndex = 0;
+    }
+    carousel.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+}
